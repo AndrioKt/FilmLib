776d53de5ea2f1c2c2f59ed6dba532a66a26a147
AndrioKt <Andrio94@yandex.ru>
2022-03-18 15:42:32 +0300

    Replacing a fragment with a recaclerView. Raw

--- app/src/main/java/com/example/filmlib/LaunchActivity.kt
+++ app/src/main/java/com/example/filmlib/LaunchActivity.kt
@@ -4,15 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.filmlib.databinding.ActivityLaunchBinding
 
-class LaunchActivity : AppCompatActivity() {
+class LaunchActivity : AppCompatActivity(), Listener {
     lateinit var binding: ActivityLaunchBinding
+    val films = generateFilms()
+    private val adapter = FilmAdapter(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLaunchBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.filmList.adapter=adapter
+        binding.filmList.layoutManager = GridLayoutManager(this, 2)
+
+
+
+
+/*
         supportFragmentManager.beginTransaction()
             .replace(R.id.MovieFrame1, MovieFragment())
             .commit()
@@ -24,7 +35,12 @@ class LaunchActivity : AppCompatActivity() {
         binding.MovieFrame1.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }
+        }*/
+    }
+
+    override fun onClick(film: Film) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
 
